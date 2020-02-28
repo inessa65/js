3ce278ce6b4cd37bd727c2ec3ec03353dd78ada1
Inessa <inessa65@yandex.by>
2020-02-28 22:54:36 +0300

    add

---
+++ .vscode/homework4/script.js
@@ -0,0 +1,4 @@
+const getRandomNumber = a => Math.round(Math.random() * 100);
+console.log(getRandomNumber());
+
+
---
+++ .vscode/homework4/script1.2.js
@@ -0,0 +1,7 @@
+function getNumber() {
+    let r = Math.round(Math.random() * 255);
+    let g = Math.round(Math.random() * 255);
+    let b = Math.round(Math.random() * 255);
+    return r + ", " + g + " ," + b;
+  }
+  console.log(getNumber());
